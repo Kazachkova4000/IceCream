eb26becb53b7911b87e3fdb7a7c7da48a1411974
Anna <kazachkova.anya@gmail.com>
2022-06-08 11:48:42 +0300

    made section

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,8 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  const headerRef = document.querySelector('[data-header]');
+
+  // const body = document.querySelector('[data-body]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
@@ -10,6 +11,6 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
-    headerRef.classList.toggle('is-fixed');
+    // body.classList.toggle('menu-open');
   });
 })();
